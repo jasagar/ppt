a406cf07461a6701c9ebdc643c6c6c6c8a20d5ed
Jaime Sansano Garcia <jasano2@hotmail.com>
2020-01-20 05:13:27 +0100

    edicion del metodo checkWinner() de la clase MoveWords.java

--- PiedraPapelTijera/src/MoveWords.java
+++ PiedraPapelTijera/src/MoveWords.java
@@ -7,8 +7,7 @@ public class MoveWords{
     public static final int PIERDE = 2;
 
     // private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA"}; // ori
-    //private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA", "LAGARTO", "ESPIA"}; // linea1
-    private static final String[] validMoves = {"TIJERAS", "LAGARTO", "PAPEL", "ESPIA", "PIEDRA"}; // linea2
+    private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA", "LAGARTO", "ESPIA"};
     private static final String[] validCommands = {"SALIR", "HELP"};
 
     private Random rnd;
@@ -62,20 +61,17 @@ public class MoveWords{
     }
 
     public static int checkWinner(String first, String second){
-	    //int first_i, second_i;//ori
-	    int first_i, second_i, longitud, resto;
+	    int first_i, second_i;
 
 	    first_i = getIndex(first);
 	    second_i = getIndex(second);
-	    longitud = validMoves.length;
-	    // resto = ((first_i +1) % longitud );
 
 	    if (first_i == second_i) return EMPATE;
 	    
 	    // return (( (first_i +1) % validMoves.length ) == second_i ) ? GANA: PIERDE;  // ori
-	    if(first_i == validMoves.length-1 && second_i == 0 ) {
+	    if(first_i + 1 == second_i || first_i - 4 == second_i){
 	    	return GANA;
-	    }else if(first_i < second_i){
+	    }else if(first_i == second_i + 2 || first_i == second_i - 3){
 	    	return GANA;
 	    }else {
 	    	return PIERDE;
